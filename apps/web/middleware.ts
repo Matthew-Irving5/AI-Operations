34e19a37f904c6e9c3275fd6a522a641c35e6a7c
Matthew-Irving5 <matthewirving99@gmail.com>
2026-08-02 23:27:29 +0100

    fix: use edge middleware for Cloudflare (#3)
    Co-authored-by: Matt-Irving <C1010636@newcastle.ac.uk>

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function proxy(request: NextRequest) {
+export const runtime = 'experimental-edge';
+
+export function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const nonce = crypto.randomUUID();
   const scriptPolicy =
