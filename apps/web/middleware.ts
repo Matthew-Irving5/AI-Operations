f8f841f866467b4cd65d5b68ce59671e7a272b6b
Matt-Irving <C1010636@newcastle.ac.uk>
2026-08-02 23:54:42 +0100

    fix: remove unsupported Cloudflare middleware runtime

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const runtime = 'experimental-edge';
-
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const nonce = crypto.randomUUID();
